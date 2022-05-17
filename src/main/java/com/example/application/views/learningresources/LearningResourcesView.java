95cf675621fcdd49e76f4d3db720aa5781d99e59
start.vaadin.com <no-reply@vaadin.com>
2022-05-17 11:47:33 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/learningresources/LearningResourcesView.java
@@ -0,0 +1,31 @@
+package com.example.application.views.learningresources;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Learning Resources")
+@Route(value = "", layout = MainLayout.class)
+public class LearningResourcesView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public LearningResourcesView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
